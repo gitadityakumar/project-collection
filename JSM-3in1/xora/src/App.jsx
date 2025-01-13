f94c53d3c828ff3707d14dcc830e1fa19a9ce2e7
gitadityakumar <itsadityasingh05@gmail.com>
2025-01-13 07:19:01 +0530

    add pricing section

--- JSM-3in1/xora/src/App.jsx
+++ JSM-3in1/xora/src/App.jsx
@@ -1,12 +1,14 @@
 import Features from "./section/Features"
 import Header from "./section/header"
 import Hero from "./section/Hero"
+import Pricing from "./section/Pricing"
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header/>
       <Hero/>
       <Features/>
+      <Pricing/>
       
 
     </main>
