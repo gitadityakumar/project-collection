9098b1fdd3d87d1ea2f1a2f2b4329f3e79772ee6
gitadityakumar <itsadityasingh05@gmail.com>
2025-01-13 11:31:15 +0530

    add testimonials,footer and wrap up this landing page drill

--- JSM-3in1/xora/src/App.jsx
+++ JSM-3in1/xora/src/App.jsx
@@ -1,8 +1,11 @@
+import Download from "./section/Download"
 import Faq from "./section/Faq"
 import Features from "./section/Features"
+import Footer from "./section/Footer"
 import Header from "./section/header"
 import Hero from "./section/Hero"
 import Pricing from "./section/Pricing"
+import Testimonials from "./section/Testimonials"
 const App = () => {
   return (
     <main className="overflow-hidden">
@@ -11,6 +14,9 @@ const App = () => {
       <Features/>
       <Pricing/>
       <Faq/>
+      <Testimonials/>
+      <Download/>
+      <Footer/>
       
 
     </main>
--- JSM-3in1/xora/src/section/Faq.jsx
+++ JSM-3in1/xora/src/section/Faq.jsx
@@ -11,7 +11,7 @@ const Faq = () => {
         <div className="container relative z-2 py-28">
           <div>
             <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
-              Curiosity didn&apos;t kill the cat, it gave it answers.
+              Curiosity didn&apos;t kill the cat, it gave the answers.
             </h3>
             <p className="body-1 max-lg:max-w-sm">
               You&apos;ve got questions, we&apos;ve got answers.
