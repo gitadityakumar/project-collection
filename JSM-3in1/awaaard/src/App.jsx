5e36d8ae237b45d4e77e3bdd0801320f4d09d591
gitadityakumar <itsadityasingh05@gmail.com>
2025-01-14 22:33:49 +0530

    add hero section

--- JSM-3in1/awaaard/src/App.jsx
+++ JSM-3in1/awaaard/src/App.jsx
@@ -1,10 +1,10 @@
-
+import Hero from './components/Hero'
 function App() {
 
   return (
-    <>
-    
-   </>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+    </main>
   )
 }
 
