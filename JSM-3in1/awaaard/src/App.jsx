bf5320f3d90dced8ecdd9c4edf4a93b5368f0f4e
gitadityakumar <itsadityasingh05@gmail.com>
2025-02-01 20:31:03 +0530

    add nav bar without animaiton

--- JSM-3in1/awaaard/src/App.jsx
+++ JSM-3in1/awaaard/src/App.jsx
@@ -1,11 +1,14 @@
 import Hero from './components/Hero'
 import About from './components/About'
+import NavBar from './components/NavBar'
 function App() {
 
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+       <NavBar/>
       <Hero/>
       <About/>
+     
     </main>
   )
 }
