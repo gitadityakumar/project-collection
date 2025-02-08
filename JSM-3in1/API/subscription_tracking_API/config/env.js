3c8943208663d7652070044f859883afdf8e0c67
Perpetuity <iamadityakumar05@proton.me>
2025-02-08 21:12:00 +0530

    Delete JSM-3in1/API/subscription_tracking_API directory

--- JSM-3in1/API/subscription_tracking_API/config/env.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-import { config } from 'dotenv';
-
-const envFile = `.env.${process.env.NODE_ENV || 'development'}.local`;
-config({ path: envFile });
-
-export const { PORT, NODE_ENV } = process.env;
