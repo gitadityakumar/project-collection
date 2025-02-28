8d05e474b023b064ffddd08e1dde8e81c7f55a6c
gitadityakumar <itsadityasingh05@gmail.com>
2025-02-28 20:50:12 +0530

    add reivews section

--- JSM-3in1/3D/portfolio/src/App.jsx
+++ JSM-3in1/3D/portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero'
 import About from './sections/About'
 import './App.css'
 import Projects from './sections/Projects'
+import Clients from './sections/Clients'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <About />
       <Projects/>
+      <Clients/>
       
     </main>
     </>
